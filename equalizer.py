daf72839d777cb8b3e309e16e2be9b2b6e9b9d3d
dv <dantev998@gmail.com>
2022-04-07 09:55:39 -0700

    cleaned up equalizer code

--- equalizer.py
+++ equalizer.py
@@ -1,41 +1,16 @@
 from dataclasses import dataclass
 import numpy as np
 import scipy.signal as sps
-import numba
 import time
 
 import config as cfg
 import equalizer_cy
-class Equalizer:
-    # todo: implement Filter in cython
-    @dataclass
-    class Filter:
-        a: np.ndarray
-        b: np.ndarray
-        a_flipped: np.ndarray
-        b_flipped: np.ndarray
-        y: np.ndarray
-        x: np.ndarray
-        nm_max: int
-        
-        def __init__(self, a: np.ndarray, b: np.ndarray):
-            self.a = a
-            self.b = b
-            self.a_flipped = np.flip(a)
-            self.b_flipped = np.flip(b)
-            self.nm_max = max(a.shape[0], b.shape[0]) - 1
-            self.x = np.zeros((cfg.buffer + self.nm_max, 2), np.double)
-            self.y = np.zeros((cfg.buffer + self.nm_max, 2), np.double)
-        
-        def filter_audio(self, audio_in):
-            audio_out, self.x, self.y = equalizer_cy.filter_audio(audio_in, self.a_flipped, self.b_flipped, self.nm_max, self.x, self.y)
-            return audio_out
-        
+class Equalizer:        
     def __init__(self):
         b_lpf, a_lpf = sps.butter(N=cfg.lowpass_order, Wn=cfg.lowpass_cutoff, fs=44100, btype='lowpass')
-        self.lpf = self.Filter(a=a_lpf, b=b_lpf)
+        self.lpf = equalizer_cy.Equalizer(a_lpf, b_lpf)
         b_hpf, a_hpf = sps.butter(N=cfg.highpass_order, Wn=cfg.highpass_cutoff, fs=44100, btype='highpass')
-        self.hpf = self.Filter(a=a_hpf, b=b_hpf)
+        self.hpf = equalizer_cy.Equalizer(a_hpf, b_hpf)
         self.bpf_cutoff = np.array(cfg.peaking_cutoff)
         self.bpf = np.empty(len(cfg.peaking_cutoff), dtype=object)
         # for i in range(len(cfg.peaking_cutoff)):
@@ -47,7 +22,7 @@ class Equalizer:
         
     def equalize(self, audio_in):
         if self.lpf_enabled==True:
-            lpf_out = self.lpf.filter_audio(audio_in)
+            lpf_out = self.lpf.filter_audio(audio_in.astype(np.double))
         else:
             lpf_out = audio_in
         if self.hpf_enabled==True:
--- equalizer_cy.pyx
+++ equalizer_cy.pyx
@@ -1,25 +1,38 @@
 import numpy as np; cimport numpy as np; np.import_array()
 import cython
 
+# user imports
+import config as cfg
 ITYPE = np.int16  # int type
 ctypedef np.int16_t ITYPE_t
 FTYPE = np.double;   # float type
 ctypedef np.double_t FTYPE_t
-@cython.boundscheck(False) # turn off bounds-checking for entire function
-@cython.wraparound(False)  # turn off negative index wrapping for entire function
 
-def filter_audio(np.ndarray[ITYPE_t, ndim=2] audio_in, np.ndarray[FTYPE_t] a_flipped, np.ndarray[FTYPE_t] b_flipped, int nm_max, np.ndarray[FTYPE_t, ndim=2] x, np.ndarray[FTYPE_t, ndim=2] y):
-    x[nm_max:x.shape[0], :] = audio_in
-    y[nm_max:y.shape[0]] = 0.0 # all values in y must equal zero or a_flipped[nm_max] must equal 0
-    cdef int i, n, j
-    a_flipped[nm_max] = 0
-    for i in range(audio_in.shape[0]):
-        n = int(i + nm_max)
-        for j in range(nm_max + 1):
-            y[n, 0] += b_flipped[j]*x[i + j, 0] - a_flipped[j]*y[i + j, 0]
-            y[n, 1] += b_flipped[j]*x[i + j, 1] - a_flipped[j]*y[i + j, 1]
-    
-    y[0:nm_max] = y[y.shape[0] - nm_max:y.shape[0]]
-    x[0:nm_max] = x[x.shape[0] - nm_max:x.shape[0]]
-    cdef np.ndarray[ITYPE_t, ndim=2] audio_out = y[nm_max:y.shape[0]].astype(np.int16)
-    return audio_out, x, y
+cdef class Equalizer:
+    cdef double[:, :] x, y
+    cdef double[:] b, a, b_flipped, a_flipped
+    cdef int nm_max
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.a_flipped = np.flip(a)
+        self.a_flipped[self.nm_max] = 0
+        self.b_flipped = np.flip(b)
+        self.nm_max = max(a.shape[0], b.shape[0]) - 1
+        self.x = np.zeros((cfg.buffer + self.nm_max, 2), np.double)
+        self.y = np.zeros((cfg.buffer + self.nm_max, 2), np.double)
+        
+    cpdef filter_audio(self, double[:, :] audio_in):
+        self.x[self.nm_max:self.x.shape[0], :] = audio_in
+        self.y[self.nm_max:self.y.shape[0]] = 0.0 # all values in y must equal zero or a_flipped[nm_max] must equal 0
+        cdef int i, n, j
+        for i in range(audio_in.shape[0]):
+            n = int(i + self.nm_max)
+            for j in range(self.nm_max + 1):
+                self.y[n, 0] += self.b_flipped[j]*self.x[i + j, 0] - self.a_flipped[j]*self.y[i + j, 0]
+                self.y[n, 1] += self.b_flipped[j]*self.x[i + j, 1] - self.a_flipped[j]*self.y[i + j, 1]
+        
+        self.y[0:self.nm_max] = self.y[self.y.shape[0] - self.nm_max:self.y.shape[0]]
+        self.x[0:self.nm_max] = self.x[self.x.shape[0] - self.nm_max:self.x.shape[0]]
+        cdef double[:, :] audio_out = self.y[self.nm_max:self.y.shape[0]]
+        return np.asarray(audio_out)
