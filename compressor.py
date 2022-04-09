7a15b6fa1f4b479e64ca450b93655901be79bc4b
dv <dantev998@gmail.com>
2022-04-09 14:32:05 -0700

    added rms to compressor

--- compressor.py
+++ compressor.py
@@ -6,30 +6,22 @@ import config as cfg
 import compressor_cy
 
 class Compressor:
-    class WriteArray:
-        def __init__(self, file_name):
-            self.f = open(file_name + '.txt', 'w')
-            
-        def write_array(self, array):
-            for i in array:
-                self.f.write(str(round(i, 3)) + "\n")
-                
-    def __init__(self, threshold=None, ratio=None, knee_width=None, pre_gain=None, post_gain=None, attack=None, release=None, log_gains=False):
+    def __init__(self, threshold=None, ratio=None, knee_width=None, pre_gain=None, post_gain=None, attack=None, release=None, rms=None, hold=None):
         self.threshold = cfg.threshold 
         self.ratio = cfg.ratio
         self.knee_width = cfg.knee_width 
-        self.pre_gain = cfg.pre_gain 
+        self.pre_gain = cfg.pre_gain
         self.post_gain = cfg.post_gain 
         self.attack = cfg.attack
         self.release = cfg.release
-        self.log_gains = log_gains
-        self.gain_logger = self.WriteArray('gain_log')
+        self.rms = cfg.rms
+        self.hold = cfg.hold
         self.update_params(threshold, ratio, knee_width, pre_gain, post_gain, attack, release)
         
     def create_compressor(self):
-        self.comp = compressor_cy.Compressor(self.threshold, self.ratio, self.knee_width, self.pre_gain, self.post_gain, self.attack, self.release)
+        self.comp = compressor_cy.Compressor(self.threshold, self.ratio, self.knee_width, self.pre_gain, self.post_gain, self.attack, self.release, self.rms, self.hold)
         
-    def update_params(self, threshold=None, ratio=None, knee_width=None, pre_gain=None, post_gain=None, attack=None, release=None):
+    def update_params(self, threshold=None, ratio=None, knee_width=None, pre_gain=None, post_gain=None, attack=None, release=None, rms=None):
         if threshold is not None: 
             self.threshold = threshold
         if ratio is not None: 
@@ -44,6 +36,8 @@ class Compressor:
             self.attack = attack
         if release is not None: 
             self.release = release
+        if rms is not None:
+            self.rms = rms
         self.create_compressor()
         
     def print_params(self):
@@ -61,8 +55,6 @@ class Compressor:
         audio_out = audio_in
         audio_out[:, 0] *= gain_linear
         audio_out[:, 1] *= gain_linear
-        if self.log_gains is True:
-            self.gain_logger.write_array(gain_linear)
         return audio_out
     
     def calculate_gain(self, audio_in):
@@ -77,7 +69,7 @@ class Compressor:
 def main():
     # plot static gain
     import matplotlib.pyplot as plt
-    comp = Compressor()
+    comp = Compressor(knee_width=50)
     db_vals = 100*np.arange(-256, 0)/256
     gc_out = comp.comp.gain_computer(db_vals)
     test = 7
@@ -86,8 +78,8 @@ def main():
     ax.plot(db_vals, gc_out, linewidth=2.0)
     plt.show()
     
-    ld_test0 = -40*np.ones(256)
-    ld_test1 = -20*np.ones(256)
+    ld_test0 = 0*np.ones(256)
+    ld_test1 = 100*np.ones(256)
     a, b, c = 2, 2, 20
     ld_out = np.zeros(1)
     for __ in range(a):
@@ -99,7 +91,7 @@ def main():
     ld_testx = np.arange(0, (a+b+c)*256)
     fig, ax = plt.subplots()
     ax.plot(ld_testx, ld_out[1:ld_out.shape[0]], linewidth=2.0)
-    ax.set(ylim=(-100, 0))
+    ax.set(ylim=(0, 100))
     plt.show()
     
 if __name__ == "__main__":
--- compressor_cy.pyx
+++ compressor_cy.pyx
@@ -10,10 +10,13 @@ ctypedef np.int16_t ITYPE_t
 FTYPE = np.double;   # float type
 ctypedef np.double_t FTYPE_t
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cdef class Compressor:
-    cdef double threshold, ratio, knee_width, pre_gain, post_gain, attack, release, atk, rel, gcp1, gcp2, gcp3, gcp4, gcp5, ldpo
-    cdef int buffer_size
-    def __init__(self, threshold, ratio, knee_width, pre_gain, post_gain, attack, release, buffer_size=cfg.buffer_size):
+    cdef double threshold, ratio, knee_width, pre_gain, post_gain, attack, release, atk, hold, rel, gcp1, gcp2, gcp3, gcp4, ldpo
+    cdef int buffer_size, hold_count, hold_counter
+    cdef bint rms
+    def __init__(self, threshold, ratio, knee_width, pre_gain, post_gain, attack, release, rms, hold, buffer_size=cfg.buffer_size):
         self.threshold = threshold
         self.ratio = ratio
         self.knee_width = (knee_width + 0.001)
@@ -21,42 +24,52 @@ cdef class Compressor:
         self.post_gain = post_gain
         self.attack = attack
         self.release = release
+        self.hold = hold
+        self.rms = rms
         self.ldpo = -100.0
         self.buffer_size = buffer_size
-        self.calculate_params()
+        self.hold_counter = 0
+        self.compute_params()
         
-    def calculate_params(self):
-        self.gcp1 = self.threshold - self.knee_width/2
-        self.gcp2 = self.knee_width/2
-        self.gcp3 = (1/self.ratio - 1)/(2*self.knee_width)
+    cpdef compute_params(self):
+        self.gcp1 = (1/self.ratio - 1)/(2*self.knee_width)
+        self.gcp2 = -self.threshold + self.knee_width/2
+        self.gcp3 = (1 - 1/self.ratio)*self.threshold
         self.gcp4 = 1/self.ratio
-        self.gcp5 = self.threshold*(1 - 1/self.ratio)
         self.atk = cmath.exp(-1/(self.attack*cfg.samplerate))
         self.rel = cmath.exp(-1/(self.release*cfg.samplerate))
+        self.hold_count = int(self.hold*cfg.samplerate)
         
     cpdef gain_computer(self, double[:] audio_lvl_in):
         cdef double[:] audio_lvl_out = np.zeros(self.buffer_size, dtype=np.double)
         cdef int i
         for i in range(self.buffer_size):
-            if 2*(audio_lvl_in[i] - self.threshold) < -self.knee_width:
+            test = 2*(audio_lvl_in[i] - self.threshold)
+            if test < -self.knee_width:
                 audio_lvl_out[i] = audio_lvl_in[i]
-            elif 2*abs(audio_lvl_in[i] - self.threshold) <= self.knee_width:
-                audio_lvl_out[i] = audio_lvl_in[i] + (1/self.ratio - 1)*cmath.pow((audio_lvl_in[i] - self.threshold + self.knee_width/2), 2)/(2*self.knee_width)
-            elif 2*(audio_lvl_in[i] - self.threshold) > self.knee_width:
-                audio_lvl_out[i] = self.threshold + (audio_lvl_in[i] - self.threshold)/self.ratio
+            elif abs(test) <= self.knee_width:
+                audio_lvl_out[i] = audio_lvl_in[i] + self.gcp1*cmath.pow((audio_lvl_in[i] + self.gcp2), 2)
+            elif test > self.knee_width:
+                audio_lvl_out[i] = self.gcp3 + audio_lvl_in[i]*self.gcp4
         return np.asarray(audio_lvl_out)
- 
+    
     cpdef level_detector(self, double[:] audio_lvl_in):
         cdef double[:] audio_lvl_out = np.zeros(self.buffer_size, dtype=np.double)
-        cdef double ldpo
+        cdef double ldpo, audio_lvl_in_i, audio_lvl_out_i
         cdef int i
         ldpo = self.ldpo
         for i in range(self.buffer_size):
-            if audio_lvl_in[i] > ldpo:
-                audio_lvl_out[i] = self.atk*ldpo + (1 - self.atk)*audio_lvl_in[i]
+            if self.rms == True: audio_lvl_in_i = audio_lvl_in[i]*audio_lvl_in[i]
+            else: audio_lvl_in_i = audio_lvl_in[i]
+            
+            if audio_lvl_in_i > ldpo:
+                audio_lvl_out_i = self.atk*ldpo + (1 - self.atk)*audio_lvl_in_i
             else:
-                audio_lvl_out[i] = self.rel*ldpo + (1 - self.rel)*audio_lvl_in[i]
-            ldpo = audio_lvl_out[i]
+                audio_lvl_out_i = self.rel*ldpo + (1 - self.rel)*audio_lvl_in_i
+                
+            if self.rms == True: audio_lvl_out[i] = cmath.sqrt(abs(audio_lvl_out_i))
+            else: audio_lvl_out[i] = audio_lvl_out_i
+            ldpo = audio_lvl_out_i
         self.ldpo = ldpo
         return np.asarray(audio_lvl_out)
     
@@ -65,7 +78,7 @@ cdef class Compressor:
         cdef int i
         for i in range(self.buffer_size):
             if audio_level_in[i]==0:
-                audio_lvl_out[i] = -99999
+                audio_lvl_out[i] = -999
             else:
                 audio_lvl_out[i] = 20*cmath.log10(abs(audio_level_in[i]))
         return np.asarray(audio_lvl_out)
